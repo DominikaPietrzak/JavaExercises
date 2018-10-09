8f1ce07ae099f84db13f87692ad399f5c73a9c79
DominikaPietrzak <dompie6@st.amu.edu.pl>
2018-10-09 13:20:16 +0200

    first

---
+++ Obiekty Request i Response/src/src/HelloServlet.java
@@ -0,0 +1,33 @@
+package src;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response){
+		String nameParam = request.getParameter("name");
+	System.out.println("Hello" + nameParam);
+
+	}
+
+}
